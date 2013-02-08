9bd37d6c7bde77c1e7ccdf94b8d9d1e6a593184a
Sonic Zhang <sonic.zhang@analog.com>
2013-02-08 17:23:58 +0800

    bf60x: dma: fix typo in last commit
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- arch/blackfin/include/asm/dma.h
+++ arch/blackfin/include/asm/dma.h
@@ -34,51 +34,71 @@ struct dmasg {
 	s16 y_modify;
 } __attribute__((packed));
 
-/*
- * All Blackfin system MMRs are padded to 32bits even if the register
- * itself is only 16bits.  So use a helper macro to streamline this.
- */
-#define __BFP(m) m; u16 __pad_##m
 struct dma_register {
 #ifdef __ADSPBF60x__
-	void *next_desc_ptr;
-	u32 start_addr;
-	u32 config;
-	u32 x_count;
-	s32 x_modify;
-	u32 y_count;
-	s32 y_modify;
+	void *next_desc_ptr;	/* DMA Next Descriptor Pointer register */
+	u32 start_addr;		/* DMA Start address  register */
+	u32 config;		/* DMA Configuration register */
+
+	u32 x_count;		/* DMA x_count register */
+	s32 x_modify;		/* DMA x_modify register */
+	u32 y_count;		/* DMA y_count register */
+	s32 y_modify;		/* DMA y_modify register */
 	u32 __pad0[2];
-	void *curr_desc_ptr;
-	void *prev_desc_ptr;
-	void *curr_addr;
-	u32 status;
-	u32 curr_x_count;
-	u32 curr_y_count;
+
+	void *curr_desc_ptr;	/* DMA Curr Descriptor Pointer register */
+	void *prev_desc_ptr;	/* DMA Prev Descriptor Pointer register */
+	void *curr_addr;	/* DMA Current Address Pointer register */
+	u32 status;		/* DMA irq status register */
+	u32 curr_x_count;	/* DMA Current x-count register */
+	u32 curr_y_count;	/* DMA Current y-count register */
 	u32 __pad1[2];
-	u32 bw_limit;
-	u32 curr_bw_limit;
-	u32 bw_monitor;
-	u32 curr_bw_monitor;
+
+	u32 bw_limit;		/* DMA Bandwidth Limit Count */
+	u32 curr_bw_limit;	/* DMA curr Bandwidth Limit Count */
+	u32 bw_monitor;		/* DMA Bandwidth Monitor Count */
+	u32 curr_bw_monitor;	/* DMA curr Bandwidth Monitor Count */
 #else
-	void *next_desc_ptr;
-	u32 start_addr;
-	u16 __BFP(config);
-	u32 __pad0;
-	u16 __BFP(x_count);
-	s16 __BFP(x_modify);
-	u16 __BFP(y_count);
-	s16 __BFP(y_modify);
-	void *curr_desc_ptr;
-	u32 curr_addr_ptr;
-	u16 __BFP(status);
-	u16 __BFP(peripheral_map);
-	u16 __BFP(curr_x_count);
-	u32 __pad1;
-	u16 __BFP(curr_y_count);
-	u32 __pad2;
+	void *next_desc_ptr;	/* DMA Next Descriptor Pointer register */
+	u32 start_addr;		/* DMA Start address  register */
+
+	u16 config;		/* DMA Configuration register */
+	u16 dummy1;		/* DMA Configuration register */
+
+	u32 reserved;
+
+	u16 x_count;		/* DMA x_count register */
+	u16 dummy2;
+
+	s16 x_modify;		/* DMA x_modify register */
+	u16 dummy3;
+
+	u16 y_count;		/* DMA y_count register */
+	u16 dummy4;
+
+	s16 y_modify;		/* DMA y_modify register */
+	u16 dummy5;
+
+	void *curr_desc_ptr;	/* DMA Current Descriptor Pointer register */
+
+	u32 curr_addr_ptr;	/* DMA Current Address Pointer register */
+
+	u16 status;		/* DMA irq status register */
+	u16 dummy6;
+
+	u16 peripheral_map;	/* DMA peripheral map register */
+	u16 dummy7;
+
+	u16 curr_x_count;	/* DMA Current x-count register */
+	u16 dummy8;
+
+	u32 reserved2;
+
+	u16 curr_y_count;	/* DMA Current y-count register */
+	u16 dummy9;
+
+	u32 reserved3;
 #endif
 };
-#undef __BFP
 
 #endif
